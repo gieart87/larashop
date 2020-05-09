b85495b962e15628525cc004303eaddb83f2ef7a
Sugiarto <sugiarto.dlingo@gmail.com>
2020-05-10 04:44:37 +0700

    Revert "added: checkout process"
    This reverts commit faa09fa294aa3ff41d5ecbd59abbe1b994ea5877.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@
  * Here is where you can register web routes for your application. These
  * routes are loaded by the RouteServiceProvider within a group which
  * contains the "web" middleware group. Now create something great!
- *
  * @category WebRoutes
  * @package  WebRoutes
  * @author   Sugiarto <sugiarto.dlingo@gmail.com>
