7111dd866c82117e100ac7fc233ea541fcb57ebe
Sugiarto <sugiarto.dlingo@gmail.com>
2020-05-29 11:21:10 +0700

    updated: fixed bug on move up the slide

--- app/Models/Slide.php
+++ app/Models/Slide.php
@@ -45,7 +45,7 @@ class Slide extends Model
 	public function prevSlide()
 	{
 		return self::where('position', '<', $this->position)
-			->orderBy('position', 'ASC')
+			->orderBy('position', 'DESC')
 			->first();
 	}
 
